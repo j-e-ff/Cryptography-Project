365417783bbf2e757328db95b61733e8f4e2b007
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2024-12-10 01:32:46 -0800

    Update MAC.java

--- MAC.java
+++ MAC.java
@@ -1,31 +1,17 @@
-import java.security.Key;
-import java.security.SecureRandom;
-
-import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MAC {
-    public static String macString(String input) throws Exception{
-        //Hardcoded key
-        String key = "S3cr3tK3y1234567";
+    public static String macString(String input, String key) throws Exception {
         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
-
-        //Creating a MAC object
         Mac mac = Mac.getInstance("HmacSHA256");
-
-        //Initializing the Mac object
         mac.init(keySpec);
 
-        //Computing the Mac
-        byte[] bytes = input.getBytes("UTF-8");      
-        byte[] macResult = mac.doFinal(bytes);
-
+        byte[] macBytes = mac.doFinal(input.getBytes("UTF-8"));
         StringBuilder hexString = new StringBuilder();
-        for (byte b : macResult) {
+        for (byte b : macBytes) {
             hexString.append(String.format("%02x", b));
         }
-
         return hexString.toString();
     }
 }
