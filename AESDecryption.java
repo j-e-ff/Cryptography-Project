231c6434f07ac2c3ccd9cf0a9e069d08e0ef46d6
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2024-12-10 01:32:13 -0800

    Update AESDecryption.java

--- AESDecryption.java
+++ AESDecryption.java
@@ -4,10 +4,8 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-//import java.security.SecureRandom;
 import java.security.spec.KeySpec;
 import java.util.Base64;
-//import java.io.UnsupportedEncodingException; 
 
 public class AESDecryption {
     private static final int KEY_LENGTH = 256;
@@ -19,18 +17,18 @@ public class AESDecryption {
             byte[] iv = new byte[16];
             System.arraycopy(encryptedData, 0, iv, 0, iv.length);
             IvParameterSpec ivspec = new IvParameterSpec(iv);
-    
+
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), ITERATION_COUNT, KEY_LENGTH);
             SecretKey tmp = factory.generateSecret(spec);
             SecretKeySpec secretKeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
-    
+
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivspec);
-    
+
             byte[] cipherText = new byte[encryptedData.length - 16];
             System.arraycopy(encryptedData, 16, cipherText, 0, cipherText.length);
-    
+
             byte[] decryptedText = cipher.doFinal(cipherText);
             return new String(decryptedText, "UTF-8");
         } catch (Exception e) {
